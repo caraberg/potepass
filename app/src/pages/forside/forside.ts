06b1a3e7ad2ba5d4712c5ca38adb07aa94e8d2f0
Drozd-LEON <brateslav.list@ukr.net>
2026-05-14 19:51:30 +0200

    reset branch to stable booking version

--- app/src/pages/forside/forside.ts
+++ app/src/pages/forside/forside.ts
@@ -5,12 +5,11 @@ import {
   renderPetSittersPage,
   setupPetSittersEvents,
 } from "../petsitters/petsitters";
-// import { renderUsers } from "./pages/users/users";
+import { renderProfile } from "../users/users";
 
 export function renderHomePage(view: HTMLElement) {
 
   view.innerHTML = `
-  
   <section class="home">
 
     <div class="hero">
@@ -62,13 +61,10 @@ export function renderHomePage(view: HTMLElement) {
   //profile
   document
     .querySelector("#profile-card")
-    ?.addEventListener("click", () => {
+    ?.addEventListener("click", async () => {
       history.pushState({}, "", "/profile");
-      view.innerHTML = `
-        <div class="profile-coming">
-          <h1>Min profil kommer snart ♡</h1>
-        </div>
-      `;
+      await renderProfile(view);
+
     });
 
   // bookings
